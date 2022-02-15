a5166402712eac4bac67191ab19b03e021febc5f
Ilya Bury <ilya.br21@gmail.com>
2022-02-15 19:09:19 +0300

    Added new table(Messages).

---
+++ UserManagerDB/UserManagerDB/Messages.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Messages]
+(
+	[MessageId] INT CONSTRAINT PK_Messages_MessageId PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[SenderId] INT NOT NULL,
+	[RecipientId] INT NOT NULL,
+	[MessageTopic] NVARCHAR(50) NOT NULL,
+	[MessageText] NVARCHAR(1000) NOT NULL
+
+	CONSTRAINT FK_Messages_Users_Sender FOREIGN KEY (SenderId) REFERENCES Users (UserId)
+	CONSTRAINT FK_Messages_Users_Recipient FOREIGN KEY (RecipientId) REFERENCES Users (UserId)
+)
