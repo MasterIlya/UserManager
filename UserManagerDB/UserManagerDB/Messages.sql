3f4ced10dac89503ad2445f55012641ccc4cb57e
Ilya Bury <ilya.br21@gmail.com>
2022-02-15 19:27:13 +0300

    Added new fields.

--- UserManagerDB/UserManagerDB/Messages.sql
+++ UserManagerDB/UserManagerDB/Messages.sql
@@ -3,8 +3,10 @@
 	[MessageId] INT CONSTRAINT PK_Messages_MessageId PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	[SenderId] INT NOT NULL,
 	[RecipientId] INT NOT NULL,
+	[CreatedDate] DATE NOT NULL,
 	[MessageTopic] NVARCHAR(50) NOT NULL,
-	[MessageText] NVARCHAR(1000) NOT NULL
+	[MessageText] NVARCHAR(1000) NOT NULL,
+	[Readed] BIT DEFAULT 0 NOT NULL
 
 	CONSTRAINT FK_Messages_Users_Sender FOREIGN KEY (SenderId) REFERENCES Users (UserId)
 	CONSTRAINT FK_Messages_Users_Recipient FOREIGN KEY (RecipientId) REFERENCES Users (UserId)
